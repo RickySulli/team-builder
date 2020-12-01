8f82596bbea7e84b37762c59d74dba792f3b7aab
RickySulli <71475646+RickySulli@users.noreply.github.com>
2020-12-01 17:53:34 -0600

    started questions

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Intern = require('../lib/Intern');
 const Manager = require('../lib/Manager');
 const fs = require()
 
-
+//make functions to elimitate repetitive if statements
 let validateInput = value =>{
     if (value != "") {
         return true;
@@ -20,8 +20,71 @@ let validateEmail = value =>{
             return "This email address is invalid!";
         }
 }
+const employees = [];
 
-const questions = [
-    
-]
-//inquirer
+function initApp(){
+    addMember();
+}
+function addMember() {
+inquirer
+    .prompt([
+    {
+        type:'input',
+        name: 'name',
+        message: "What is the employee's name?",
+        validate: validateInput
+    },
+    {
+        type: 'number',
+        name: 'id',
+        message: "What is the employee's id?",
+        validate: validateInput
+
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the employee's email address?",
+        validate: validateInput
+    },
+    {
+        type:'list',
+        name:'role',
+        message: "What is the employee's role?",
+        choices: [
+            "Intern",
+            "Manager",
+            "Engineer"
+        ],
+        validate: validateInput
+    }])
+    .then(function({name, id, email, role}){
+        let roleInfo = "";
+        if (role === "Engineer") {
+            roleInfo = "GitHub username";
+        } else if (role === "Intern") {
+            roleInfo = "school name";
+        }else{
+            roleInfo = "office phone number"
+        }
+            inquirer.prompt([
+            {   
+                type: 'input',
+                name: 'roleInfo',
+                message: `Enter team member's ${roleInfo}`
+
+            },
+            // {   
+            //     name: 'newMember',
+            //     type: 'list',
+            //     message: "Would you like to add more members to your team?",
+            //     choices: [
+            //         "YES",
+            //         "NO"
+            //     ]
+            
+            // },
+        ])
+
+    }
+}
