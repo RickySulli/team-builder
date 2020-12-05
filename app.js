3032c997975d977cf262e53f33164c26bdd7ee4a
RickySulli <71475646+RickySulli@users.noreply.github.com>
2020-12-05 17:40:59 -0600

    assistance

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const fs = require('fs');
-const path = require('path');
+const generatePage = require('./src/htmlrender')
+const validate = require('./validate')
 
+
+const team = [];
 //make functions to elimitate repetitive if statements
 let validateInput = value =>{
     if (value != "") {
@@ -21,108 +24,151 @@ let validateEmail = value =>{
             return "This email address is invalid!";
         }
 }
-let validatePhone = (inputtxt) => {
-    let phone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-        if(inputtxt.value.match(phone)){
-            return true
-        }else{
-            return "Enter a valid Phone Number! (XXX-XXX-XXXX)"
-        }
+let validatePhone = value => {
+    if(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(value)){
+        return true
+    }else{
+        return "please enter a 10 digit phone number"
+    }
+}
+let validateNumber = value =>{
+   if(/^\d{6}(\s*,\s*\d{6})*$/.test(value)){
+       return true
+   }else{
+       return "please enter a six digit number"
+   }
 }
 //prompt user to create team manager.
-const promptUser = () => {
-    const createManager = () => {
-        inquirer.prompt([
-            {
-                name: "managerName",
-                type: 'input',
-                message: "What is your team manager name?",
-                validate: validateInput
-            },
-            {
-                name: 'managerId',
-                type: 'number',
-                message: "What is your team manager ID number?",
-                validate: positiveNumber
-            },
-            {
-                name: 'managerEmail',
-                type: 'input',
-                message: 'What is your team manager email address?',
-                validate: validateEmail
-            },
-            {
-                name: 'officeNumber',
-                type: 'input',
-                message: "What's your office Number?",
-                validate: validatePhone
-            }
-        ])
-    }
-}
-const teamMembers = () => {
-return inquirer
-    .prompt([
-    {
-        type:'list',
-        name:'role',
-        message: "What is the employee's role?",
-        choices: [
-            "Intern",
-            "Engineer"
-        ],
-        validate: validateInput
-    },
-    {
-        type:'input',
-        name: 'name',
-        message: "What is the employee's name?",
-        validate: validateInput
-    },
-    {
-        type: 'number',
-        name: 'id',
-        message: "What is the employee's id?",
-        validate: validateInput
-
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is the employee's email address?",
-        validate: validateInput
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your github username?',
-        when: (answers) => answers.role === 'Engineer'
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: "What school did the intern attend?",
-        when: (answers) => answers.role === 'Intern'
-    },
-    {
-        type: 'confirm',
-        name: 'moreEmployees',
-        message: "Would you like to add additional employees?",
-        default: false
-    },
-    {
-        type: 'list',
-        name: 'additionalRole',
-        message: "What type of employee would you like to add?",
-        when: (answers) => answers.moreEmployees === true,
-        choices: [
-            'Manager',
-            'Engineer',
-            'Intern'
-        ]
-
-    }
-    ]).then(inputData=> {
-
+const createManager = () => {
+    inquirer.prompt([
+        {
+            name: "managerName",
+            type: 'input',
+            message: "What is your team manager name?",
+            validate: validateInput
+        },
+        {
+            name: 'managerId',
+            type: 'number',
+            message: "What is your team manager ID number?",
+            validate: validateNumber 
+        },
+        {
+            name: 'managerEmail',
+            type: 'input',
+            message: 'What is your team manager email address?',
+            validate: validateEmail
+        },
+        {
+            name: 'officeNumber',
+            type: 'input',
+            message: "What's your office Number?",
+            validate: validatePhone
+        }
+                    
+    ]) .then(mAnswers => {
+        let manager = new Manager(mAnswers.managerName, mAnswers.managerId, mAnswers.managerEmail, mAnswers.officeNumber)
+        team.push(manager)
+        //console.log(team);   
     })
-} 
+
+};
+
+const createTeam = (teamData) => {
+    //if there isn't a team array create one
+    // if(!teamData.info){
+    //     teamData.info =[];
+    // }
+    return inquirer
+        .prompt([
+        {
+            type:'list',
+            name:'role',
+            message: "What type of employee would you like to add?",
+            choices: [
+                "Intern",
+                "Engineer",
+                "I am done adding employees"
+            ],
+            validate: validateInput
+        }
+    ])
+    .then(answer => {
+        if(answer.choices === 'Intern'){
+            createIntern()
+        }else if(answer.choices === "Engineer"){
+            createEngineer()
+        }else if(answer.choices === 'I am done adding employees'){
+            console.log(team);
+        }
+    })
+}
+const createIntern = () => {
+    inquirer .prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: "What is the intern's name?",
+            validate: validateInput
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: "What is the intern's id?",
+            validate: validateInput
+
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the intern's email address?",
+            validate: validateInput
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What school did the intern attend?",
+        }
+    ]).then(iAnswers => {
+        let intern = new Intern(iAnswers.name, iAnswers.id, iAnswers.email, iAnswers.school)
+            team.push(intern)
+            return createTeam();
+    })
+};
+const createEngineer = () => {
+    inquirer.prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: "What is the Engineer's name?",
+            validate: validateInput
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: "What is the Engineer's id?",
+            validate: validateInput
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the Engineer's email address?",
+            validate: validateInput
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: "What is the Engineer's github username?"
+        }
+    ]).then(eAnswers => {
+        let engingeer = new Engineer(eAnswers.name, eAnswers.id, eAnswers.email, eAnswers.username)
+            team.push(engineer)
+            return createTeam();
+    })
+}
+
+createManager()
+    
+
+
+  
